94e5e24754c3e5528c339ec6cf10adc5ba9634d7
siyficocoa <messenger@siyfico.cloud>
2022-04-20 02:29:04 +0800

    passof9 java

---
+++ java/of/of9-StackQueue.java
@@ -0,0 +1,35 @@
+// 2022-04-20 02:26:57
+// Runtime 1150ms(6.9%)
+// Memory 48mb(69.0)
+
+class CQueue {
+    Stack<Integer> in;
+    Stack<Integer> out;
+    Integer ret;
+    public CQueue() {
+        this.in = new Stack();
+        this.out = new Stack();
+    }
+    
+    public void appendTail(int value) {
+        this.in.push(value);
+    }
+    
+    public int deleteHead() {
+        if(this.in.empty())
+            return -1;
+        while(!this.in.empty())
+            this.out.push(this.in.pop());
+        this.ret = this.out.pop();
+        while(!this.out.empty())
+            this.in.push(this.out.pop());
+        return ret.intValue();
+    }
+}
+
+/**
+ * Your CQueue object will be instantiated and called as such:
+ * CQueue obj = new CQueue();
+ * obj.appendTail(value);
+ * int param_2 = obj.deleteHead();
+ */
