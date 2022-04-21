0edf09fa26d8bf32967155843480060761d3de57
siyficocoa <messenger@siyfico.cloud>
2022-04-21 17:42:18 +0800

    pass58 py3

---
+++ py3/58-LengthOfLastWord.py
@@ -0,0 +1,11 @@
+# 2022-04-21 17:40:43
+# Runtime 40ms(36.3%)
+# Memory 14.9mb(76.9%)
+
+##
+# 这是作弊！
+###
+
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        return len(s.split()[-1])
