4d1eb70e15a54237453d9ca05f814cd01afe72f2
siyficocoa <messenger@siyfico.cloud>
2022-04-22 17:32:04 +0800

    pass319 java cpp

---
+++ cpp/319-BulbSwitcher.cpp
@@ -0,0 +1,10 @@
+// 2022-04-22 17:31:16
+// Runtime 0ms(100.0%)
+// Memory 5.8mb(74.0)
+
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        return (int)sqrt(n);
+    }
+};
---
+++ java/319-BulbSwitcher.java
@@ -0,0 +1,12 @@
+// 2022-04-22 17:27:45
+// Runtime 0ms(100.0%)
+// Memory 38.4mb(15.1%)
+
+/**
+ * 中等是吧
+ */
+
+class Solution {
+    public int bulbSwitch(int n) {
+        return (int)Math.sqrt(n);
+    }
