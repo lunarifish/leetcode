ac835cd79e96eefd8759d4b0eb3afab92278a257
siyficocoa <messenger@siyfico.cloud>
2022-05-06 03:30:08 +0800

    update933 py3

--- py3/933-NumberOfRecentCalls.py
+++ py3/933-NumberOfRecentCalls.py
@@ -20,3 +20,35 @@ class RecentCounter:
 # Your RecentCounter object will be instantiated and called as such:
 # obj = RecentCounter()
 # param_1 = obj.ping(t)
+
+
+"""---------------------------------------------"""
+
+
+# 2022-5-6 03:26
+# Runtime 3368ms(5.0%)
+# Memory 19.7mb(75.8%)
+
+##
+# 史诗级升级（不
+###
+
+class RecentCounter:
+
+    def __init__(self):
+        self.l = list()
+
+    def ping(self, t: int) -> int:
+        self.l.append(t)
+        counter = 0
+        for i in self.l[::-1]:
+            if i >= t - 3000:
+                counter += 1
+            else:
+                break
+        return counter
+
+
+# Your RecentCounter object will be instantiated and called as such:
+# obj = RecentCounter()
+# param_1 = obj.ping(t)
