aee44d6a07f41657e1af4ab02a4f3b70047c2f30
siyficocoa <messenger@siyfico.cloud>
2022-05-08 16:33:59 +0800

    update442 cpp

--- cpp/442-FindAllDublicatesInAnArray.cpp
+++ cpp/442-FindAllDublicatesInAnArray.cpp
@@ -19,3 +19,34 @@ public:
         return ret;
     }
 };
+
+
+/*---------------------------------------------*/
+
+
+// 2022-5-8 16:31
+// Runtime 36ms(93.7%)
+// Memory 32.7mb(70.6%)
+
+/**
+ * 题解
+ * 那个while循环的作用是什么 我不理解
+ * swap只是把两个元素来回换吧
+ */
+
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        int i;
+        vector<int> ret;
+        for(i = 0; i < nums.size(); ++i) {
+            while (nums[i] != nums[nums[i] - 1])
+                swap(nums[i], nums[nums[i] - 1]);
+        }
+        for(i = 0; i < nums.size(); ++i) {
+            if (i != nums[i] - 1)
+                ret.push_back(nums[i]);
+        }
+        return ret;
+    }
+};
