c888fcc2eca28ae58a55f21a3a94c91c7b24af2d
额 <hnliu_2@stu.xidian.edu.cn>
2024-11-19 14:32:36 +0800

    804 py3

--- py3/804-UniqueMorseCodeWords.py
+++ py3/804-UniqueMorseCodeWords.py
@@ -42,3 +42,42 @@ class Solution:
                 buffer += morse[char]
             res.add(buffer)
         return len(res)
+        
+        
+# 2024-11-19
+# Runtime 4ms(2.2%)
+# Memory 16.4mb(63.8%)
+
+# 怎么nm的runtime短了这么多
+# 而且还只有2.2%了
+
+class Solution:
+    def uniqueMorseRepresentations(self, words: List[str]) -> int:
+        morse = {"a": ".-",
+                 "b": "-...",
+                 "c": "-.-.",
+                 "d": "-..",
+                 "e": ".",
+                 "f": "..-.",
+                 "g": "--.",
+                 "h": "....",
+                 "i": "..",
+                 "j": ".---",
+                 "k": "-.-",
+                 "l": ".-..",
+                 "m": "--",
+                 "n": "-.",
+                 "o": "---",
+                 "p": ".--.",
+                 "q": "--.-",
+                 "r": ".-.",
+                 "s": "...",
+                 "t": "-",
+                 "u": "..-",
+                 "v": "...-",
+                 "w": ".--",
+                 "x": "-..-",
+                 "y": "-.--",
+                 "z": "--.."}
+        return len(set(["".join([morse[i] for i in s]) for s in words]))
+    
